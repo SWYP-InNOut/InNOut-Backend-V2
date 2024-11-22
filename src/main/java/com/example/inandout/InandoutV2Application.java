faefa61e6a85d03f4f9f00f3c1362350d670bc9f
정소민 <yjk70702002@gmail.com>
2024-11-22 15:04:57 +0900

    first commit

---
+++ src/main/java/com/example/inandout/InandoutV2Application.java
@@ -0,0 +1,13 @@
+package com.example.inandout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InandoutV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InandoutV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inandout/InandoutV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inandout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InandoutV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
