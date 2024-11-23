b34fe244548ef6b1e18d634e5bacdd02b8b5c2c1
정소민 <yjk70702002@gmail.com>
2024-11-23 19:42:42 +0900

    chore: build.gradle 파일 정리

--- src/main/java/com/example/inandout/InandoutV2Application.java
+++ src/main/java/com/example/inandout/InandoutV2Application.java
@@ -1,13 +1,16 @@
 package com.example.inandout;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class InandoutV2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(InandoutV2Application.class, args);
+        log.info("hello inandout");
     }
 
 }
