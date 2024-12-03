cfadb4ee548a71f74952448150a922bb74d40843
정소민 <yjk70702002@gmail.com>
2024-12-03 22:33:32 +0900

    feat(#2): 로그인 실패 응답

--- src/main/java/com/example/inandout/global/auth/filter/LoginFilter.java
+++ src/main/java/com/example/inandout/global/auth/filter/LoginFilter.java
@@ -98,5 +98,11 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         writer.close();
     }
 
-
+    //로그인 실패시 실행하는 메소드
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        //로그인 실패시 401 응답 코드 반환
+        System.out.println("unsuccessfulAuthentication: " + failed.getMessage());
+        response.setStatus(401);
+    }
 }
