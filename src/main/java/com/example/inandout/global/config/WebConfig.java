7eea3d462646412d5306a3b525a8b897da5fd02f
정소민 <yjk70702002@gmail.com>
2024-12-10 21:05:21 +0900

    feat(#2): cors 설정

---
+++ src/main/java/com/example/inandout/global/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.example.inandout.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .exposedHeaders("Authorization", "Member", "Set-Cookie")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
